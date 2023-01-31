941d5ab4b69a5fff17a4ab02c6b784adbad5bd88
tanguy-rdt <tanguy@roudaut.xyz>
2023-01-31 19:13:59 +0100

    assembleur -- end codeop with 3 args + stop

--- assembleur/assembleur.py
+++ assembleur/assembleur.py
@@ -1,9 +1,42 @@
+#TODO: jmp, braz, branz, scall
+#TODO: negative value with imm
+
 import re
 
+opcode = {}
+opcode["stop"]  = 0
+opcode["add"]   = 1
+opcode["sub"]   = 2
+opcode["mul"]   = 3
+opcode["div"]   = 4
+opcode["and"]   = 5
+opcode["or"]    = 6
+opcode["xor"]   = 7
+opcode["shl"]   = 8
+opcode["shr"]   = 9
+opcode["slt"]   = 10
+opcode["sle"]   = 11
+opcode["seq"]   = 12
+opcode["load"]  = 13
+opcode["store"] = 14
+
+register = {}
+register["r0"] = 0
+register["r1"] = 1
+register["r2"] = 2
+register["r3"] = 3
+register["r4"] = 4
+register["r5"] = 5
+register["r6"] = 6
+register["r7"] = 7
+register["r8"] = 8
+register["r9"] = 9
+
+
 def load_asm(asm_file):
-    comment = re.compile(r"^#")
+    comment = re.compile(r"^\s*#")
     label = re.compile(r"^\w")
-    linebreak = re.compile(r"\n")
+    linebreak = re.compile(r"\n|\r")
 
     with open(asm_file, "r") as fp:
         data = []
@@ -12,19 +45,53 @@ def load_asm(asm_file):
                     continue
             else:
                 line = line.replace(',', ' ')
-                line = line.split()   
+                line = line.split()  
                 if '#' in line:
                     line = line[:line.index('#')]
-                
-            data.append(line)
-        
+                data.append(line)
     return data
+
+
+def open_bin_file():
+    fd = open("data.bin", "w")
+    return fd
+
+def get_instr_hex(instr_text):
+    imm = 0
+    if instr_text[0] == "stop":
+        instr = 0
+        instr = '0x{0:08X}'.format(instr)
+        return instr
+    
+    if not instr_text[2] in register:
+        imm = 1
+        
+    instr = opcode.get(instr_text[0]) << 27
+    instr += register.get(instr_text[1]) << 22
+    instr += imm << 21
+    if imm:
+        instr += int(instr_text[2]) << 5
+    else :
+        instr += register.get(instr_text[2]) << 5
+    instr += register.get(instr_text[3])
+    instr = '0x{0:08X}'.format(instr)
+    
+    return instr
+
+def append_to_bin_file(fd, instr_num, instr_hex):
+    instr_num_hex = '0x{0:08X}'.format(instr_num)
+    line = instr_num_hex + ' ' + instr_hex
+    fd.write(line + "\n")    
+
+
     
 def main():
-    data = load_asm("data.asm")
-    print(data)
-    
-
+    instrs = load_asm("data.asm")
+    fd_binary = open_bin_file()
+    for instr in instrs:
+        instr_hex = get_instr_hex(instr)
+        append_to_bin_file(fd_binary, instrs.index(instr), instr_hex)
+        
 
 if __name__ == '__main__':
     main()        
--- assembleur/data.asm
+++ assembleur/data.asm
@@ -1,5 +1,4 @@
 #test 1 asm : dummy code to test all instruction conversion
-
         add  r7, r2,r3    # presenté dans le slideware 0x09c00043
         add  r1, r2,r3    # additionner c'est bien
         sub  r4,  5,r6
@@ -16,10 +15,11 @@ label1:
 
 label2: 
         store r7, 18,r9
-        braz  r0, label1
-        branz r2, label2
+#        braz  r0, label1
+#        branz r2, label2
         sub r3,-42,r3
+
 label3:
-        jmp r2, r0
+ #       jmp r2, r0
         stop              # hey
         add r1,-1,r2
