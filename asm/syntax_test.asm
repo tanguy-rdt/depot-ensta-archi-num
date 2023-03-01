3593912d1c9b2f02ab14b4a877ef389059f848a6
tanguy-rdt <tanguy@roudaut.xyz>
2023-03-01 22:56:09 +0100

    asm -- add syntax test

---
+++ asm/syntax_test.asm
@@ -0,0 +1,19 @@
+; comment add  r0, 4,  r31 
+
+label1: sub  r31, 2,  r31
+        sub  r2, r1, r4
+
+label2: sub  r31, 2,  r31 ; comment
+        sub  r2, r1, r4
+
+label3:; comment
+        sub  r2, r1, r4
+
+label4: ; comment
+        sub  r2, r1, r4
+
+label5:
+        add  r1, r2, r3
+        add  r0, 4,  r31    ; comment
+
+        stop
