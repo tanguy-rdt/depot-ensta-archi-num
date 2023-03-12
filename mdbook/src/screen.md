f430c4769ad5c6ae3274f110ee4ffbded022a5fb
tanguy-rdt <tanguy@roudaut.xyz>
2023-03-12 13:08:20 +0100

    mdbook -- update

--- mdbook/src/screen.md
+++ mdbook/src/screen.md
@@ -19,6 +19,10 @@ Registre d'initialisation :
 
 ![](./.img/screen_reg_init.png)
 
+- `f` : Facteur de taille, permet d'agrandir la taille d'un pixel
+- `x` : Nombre de pixels en largeur
+- `y` : Nombre de pixels en hauteur 
+
 #### Affichage
 Si l'écran est initaliser avec _n_ pixel, alors les _n_ lignes qui suivent le registre d'initalisation sont reservé à la mémoire. 
 C'est un écran monochrome, au moment de l'initalisation tout les pixels sont configurer en blanc. Il y a seulement les pixels donné en mémoire à la suite du registre d'initalisation seront de couleur noir.
@@ -27,6 +31,9 @@ Registre d'affichage :
 
 ![](./.img/screen_reg_aff.png)
 
+- `x` : Position x du pixel à mettre en noir
+- `y` : Position y du pixel à mettre en noir
+  
 Par exemple si on veut le pixel en _x=5_ et _y=5_ alors on doit mettre en mémoire: `0x00005005`
 
 
@@ -38,14 +45,19 @@ A chaque modification de la mémoire, l'écran réalise un lecture des registres
 
 Cette écran fonctionne, quand on on réalise une modification de la mémoire à la main et qu'on l'enregistre. Les modifications sont bien apporté sur l'écran. \
 Contrairement avec l'ISS, ce problème vient de notre méthode de lecture et d'écriture dans la mémoire. Dans notre fichier `memory.cpp`, on ouvre une seul fois la mémoire, quand on écris dedans les modifications sont bien réalisé. Mais comme on ne le ferme jamais 
-les modifications ne sont pas enregistrer, elle reste dans le buffer, alors l'écran ne peut pas détecter une modification. Il aurait fallu mmodifier notre méthode ; ouvrir et fermer la mémoire à chaque modification, mais par manque de temps nous n'avons pas pus réaliser c'est modifications.
+les modifications ne sont pas enregistrer, elle reste dans le buffer, alors l'écran ne peut pas détecter une modification. Il aurait fallu modifier notre méthode ; ouvrir et fermer la mémoire à chaque modification, mais par manque de temps nous n'avons pas pus réaliser c'est modifications.
 
 #### Initialisation 
 La première ligne de la mémoire, adresse `0x00000000`, permet d'initialiser l'écran.
 
 Registre d'initialisation :
 
-![](./.img/screen_reg_init.png)
+![](./.img/screen_v0_reg_init.png)
+
+- `g` : Affichage d'une grille
+- `f` : Facteur de taille, permet d'agrandir la taille d'un pixel
+- `x` : Nombre de pixels en largeur
+- `y` : Nombre de pixels en hauteur 
 
 #### Affichage
 Si l'écran est initaliser avec _n_ pixel, alors les _n_ lignes qui suivent le registre d'initalisation sont reservé à la mémoire. 
@@ -53,6 +65,10 @@ C'est un écran monochrome, au moment de l'initalisation tout les pixels sont co
 
 Registre d'affichage :
 
-![](./.img/screen_reg_aff.png)
+![](./.img/screen_v0_reg_aff.png)
 
-Par exemple si on veut le pixel en _x=5_ et _y=5_ alors on doit mettre en mémoire: `0x00005005`
+- `x` : Position x du pixel à mettre en couleur
+- `y` : Position y du pixel à mettre en couleur
+- `color` : Couleur du pixel en hexadécimal
+
+Par exemple si on veut le pixel en _x=5_ et _y=5_ de couleur _gris_ alors on doit mettre en mémoire: `0x55E8E9EB`
---
+++ simulateur/screen/screen_v0.py
@@ -0,0 +1,108 @@
+import os
+import time
+
+from PySide6.QtCore import QFileSystemWatcher
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
+from PySide6.QtGui import QPixmap, QColor, QImage, QPainter, QPen
+
+        
+class Memory():
+    def __init__(self, memory_path):
+        self.data_memory = []
+        self.memory_path = memory_path
+        self.read_memory()
+    
+    def error(self):
+        pass
+    
+    def read_memory(self):
+        try:
+            self.data_memory = []
+            memory_fd = open(self.memory_path, "r")
+            for line in memory_fd:
+                line = line.split()
+                self.data_memory.append(int(line[1], 16))
+            memory_fd.close()
+        except Exception as err:
+            self.error()
+        
+class Screen(QMainWindow):
+    def __init__(self, mem):                
+        super().__init__()
+        
+        assert isinstance(mem, Memory)
+        self.memory = mem
+        self.width, self.height, self.size_factor, self.grid_state = self.get_init_parameter(self.memory.data_memory)
+        self.size_factor = 30
+        self.pixels = []
+    
+    def update_image(self):
+        image = QImage(self.width, self.height, QImage.Format_RGB32)
+        for px in self.pixels:
+            r = (px.get("color") >> 16) & 0x000000ff
+            g = (px.get("color") >> 8) & 0x000000ff
+            b = px.get("color") & 0x000000ff
+            image.setPixelColor(px.get('x'), px.get('y'), QColor(r, g, b))
+        return image
+    
+    def get_init_parameter(self, data_memory):
+        width = data_memory[0] & 0x0000000f
+        height = (data_memory[0] >> 4) & 0x0000000f
+        size_factor = (data_memory[0] >> 8) & 0x000000ff 
+        grid_state = (data_memory[0] >> 16) & 0x0000000f
+        return width, height, size_factor, grid_state
+    
+    def get_px(self, data_memory):
+        for px in data_memory[1:(self.width*self.height)+self.width+1]:
+            px_parameter = {}
+            px_parameter["color"] = px & 0x00ffffff
+            px_parameter["y"] = (px >> 24) & 0x0000000f
+            px_parameter["x"] = (px >> 28) & 0x0000000f
+            self.pixels.append(px_parameter)
+            
+    def image_to_pixmap(self, image):
+        pixmap = QPixmap.fromImage(image).scaled(self.width*self.size_factor, self.height*self.size_factor)
+        return pixmap
+        
+    def add_grid(self, pixmap):
+        painter = QPainter(pixmap)
+        pen = QPen(QColor('gray'))
+        pen.setWidth(1)
+        painter.setPen(pen)
+        for x in range(self.width):
+            painter.drawLine(x * self.size_factor, 0, x * self.size_factor, self.height * self.size_factor)
+        for y in range(self.height):
+            painter.drawLine(0, y * self.size_factor, self.width * self.size_factor, y * self.size_factor)
+        painter.end()
+        
+        return pixmap
+        
+    def show_pixmap(self, pixmap):
+        label = QLabel(self)
+        label.setPixmap(pixmap)
+        self.setCentralWidget(label)
+        
+def on_memory_modified(memory_path, mem, screen):
+    mem.read_memory()
+    screen.get_px(mem.data_memory)
+    image = screen.update_image()
+    pixmap = screen.image_to_pixmap(image)
+    screen.add_grid(pixmap)
+    screen.show_pixmap(pixmap)
+    screen.show()
+    
+def main():
+    app = QApplication()
+    
+    memory_path = os.path.join(os.getcwd(), "memory/.memory.bin")
+    print(memory_path)
+    mem = Memory(memory_path)
+    screen = Screen(mem)
+    on_memory_modified(memory_path, mem, screen)
+    watcher = QFileSystemWatcher([memory_path])
+    watcher.fileChanged.connect(lambda: on_memory_modified(memory_path, mem, screen))
+    
+    app.exec()
+
+if __name__ == '__main__':
+    main()
